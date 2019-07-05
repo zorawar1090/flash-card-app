f4f918b3f4049b61cac7b1416ffb6777325ce5d0
zorawar1090 <zorawarsingh9988@gmail.com>
2019-07-05 12:30:53 +0200

    User Story 5 - Add button-Next to avoid reloading

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ const data = {
     answers: ['Tuin', 'Tafel', 'Stoel', 'Auto', 'Raam']
 }
 
-const rand = data.questions[Math.floor(Math.random() * data.questions.length)]
+
 
 const h2 = document.createElement('h2')
-h2.innerHTML = rand;
+h2.innerHTML = data.questions[Math.floor(Math.random() * data.questions.length)]
 const box = document.getElementById('box')
 
 box.appendChild(h2);
 
+function randQuestion(){
+    rand = data.questions[Math.floor(Math.random() * data.questions.length)]
+    h2.innerHTML = rand;
+}
+
 function showAnswer(){
     for(let i = 0; i < data.questions.length; i++){
-        if(rand == data.questions[i]){
+        if(h2.innerHTML == data.questions[i]){
             h2.innerHTML = null
             h2.innerHTML = data.answers[i]
         }
@@ -26,4 +31,7 @@ const answer = document.getElementById('answer')
 function addData() {
     data.questions.push(question.value)
     data.answers.push(answer.value)
+
+    question.value = null
+    answer.value = null
 }
